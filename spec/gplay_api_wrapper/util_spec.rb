5e45131c8674ffa5f44bef25e3edd94d58e94b4a
filipned <filipned@live.com>
2018-05-31 13:59:59 +0200

    Done some refactoring.

--- spec/gplay_api_wrapper/util_spec.rb
+++ spec/gplay_api_wrapper/util_spec.rb
@@ -1,15 +1,16 @@
+require 'gplay_api_wrapper/util'
 RSpec.describe GplayApiWrapper::Util do
 
   describe '.to_query' do
-    subject { described_class.to_query(qs) }
-    let(:qs) do
+    subject { described_class.to_query(query_string) }
+    let(:query_string) do
       { key: 'value', field: 'value' }
     end
 
     it { is_expected.to eq('key=value&field=value') }
 
     context 'when key is -1' do
-      let(:qs) do
+      let(:query_string) do
         { key: -1, field: 'value' }
       end
       it 'is removed from the QS' do
@@ -17,5 +18,4 @@ RSpec.describe GplayApiWrapper::Util do
       end
     end
   end
-
 end
