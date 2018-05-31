4df40646ea2ee00f3dbb8f3892771bca66e6a7c4
gc-cjo <filip.nedovic@gamecredits.com>
2018-05-31 17:18:56 +0200

    Implemented tests for AgeRestrictions, Apps, RequestBuilder, RequestResolver, Base classes.

---
+++ spec/gplay_api_wrapper/endpoints/apps_spec.rb
@@ -0,0 +1,28 @@
+require 'rspec'
+require 'gplay_api_wrapper/endpoints/apps'
+
+RSpec.describe GplayApiWrapper::Apps do
+  describe '.get  [200, 299]' do
+    subject { described_class.get(query_string) }
+    let(:query_string) { {limit: 1} }
+    it 'should return' do
+      expect(subject.code.to_i/100).to eq(2)
+    end
+  end
+  describe '.get_app' do
+    subject { described_class.get_app(app_id, query_string) }
+    let(:app_id) { 6 }
+    let(:query_string) { {limit: 1} }
+    it 'should return [200, 299]' do
+      expect(subject.code.to_i/100).to eq(2)
+    end
+  end
+  describe '.get_app_version' do
+    subject { described_class.get_app_versions(app_id, query_string) }
+    let(:app_id) { 6 }
+    let(:query_string) { {limit: 1} }
+    it 'should return [400, 499]' do
+      expect(subject.code.to_i/100).to eq(4)
+    end
+  end
+end
